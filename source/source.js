b2a99d84e9fd8ee8d6fe9eb10cad2a353d073841
yobin kumar pun <punyobin123@gmail.com>
2019-11-23 20:55:34 +0545

    Modal for Menu

--- source/source.js
+++ source/source.js
@@ -1,12 +1,20 @@
 var navLink = document.getElementById("navIcon");
 var crossLink = document.getElementById("crossIcon");
+var modalShow = document.querySelector(".modal");
+var gymBody = document.querySelector("body");
 
 navLink.onclick = function() {
     navLink.style.display = "none";
     crossLink.style.display = "block";
+    modalShow.style.display = "flex";
+    //hide body overflow style to hide side scroll bar
+    gymBody.style.overflow = "hidden";
+
 }
 
 crossLink.onclick = function() {
     crossLink.style.display = "none";
     navLink.style.display = "block";
+    modalShow.style.display = "none";
+    gymBody.style.overflow = "auto";
 }
